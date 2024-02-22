ae72d8ea2b876c026b37179f890dd55bb4889489
Joseal19 <albertoj0988@gmail.com>
2024-02-21 22:43:03 -0300

    Atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,6 @@ import './App.css';
 import AppRouter from "./router/AppRouter";
 import { BrowserRouter } from 'react-router-dom';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCfKeruZPDMTp7oVSgOU5ZqO5rgjpoqHL4",
-  authDomain: "dashboard-5872b.firebaseapp.com",
-  projectId: "dashboard-5872b",
-  storageBucket: "dashboard-5872b.appspot.com",
-  messagingSenderId: "809274088054",
-  appId: "1:809274088054:web:d873b5dc4db7b126289d57",
-  measurementId: "G-WTJWV0G9GH"
-};
-
-const app = initializeApp(firebaseConfig);
-
 
 function App() {
   return (
