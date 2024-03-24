8fea892156c3b55ca40bd075705d25851a9682fd
Joseal19 <albertoj0988@gmail.com>
2024-03-24 13:30:53 -0300

    atualizando

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
 import { Badge } from 'primereact/badge';
+import { InputText } from 'primereact/inputtext';
 
 const ShoppingCart = ({ produtosNoCarrinho, removerDoCarrinho }) => {
     const [sidebarVisible, setSidebarVisible] = useState(false);
+    const [endereco, setEndereco] = useState('');
+    const [cidade, setCidade] = useState('');
 
     const openSidebar = () => {
         setSidebarVisible(true);
@@ -18,19 +21,22 @@ const ShoppingCart = ({ produtosNoCarrinho, removerDoCarrinho }) => {
         return produtosNoCarrinho.reduce((total, produto) => total + produto.price, 0).toFixed(2);
     };
 
-    // Função para construir o link do WhatsApp com os produtos e valores do pedido
+    // Função para construir o link do WhatsApp com os produtos, valores do pedido e informações de endereço
     const buildWhatsAppLink = () => {
         const mensagem = `Olá, gostaria de fazer o seguinte pedido:\n`;
         const produtos = produtosNoCarrinho.map(produto => `${produto.name} - R$ ${produto.price.toLocaleString('pt-BR')}`).join('\n');
         const total = `\nTotal: R$ ${calcularTotal().toLocaleString('pt-BR')}`;
-    
+        const enderecoMsg = endereco ? `\nEndereço: ${endereco}` : '';
+        const cidadeMsg = cidade ? `\nCidade: ${cidade}` : '';
+
         // Número de telefone do WhatsApp
         const numeroWhatsApp = '5544999108873';
-    
+
         // Montando o link
-        const link = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem + produtos + total)}`;
+        const link = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem + produtos + total + enderecoMsg + cidadeMsg)}`;
+        
         return link;
-    };    
+    };
 
     return (
         <div>
@@ -56,6 +62,22 @@ const ShoppingCart = ({ produtosNoCarrinho, removerDoCarrinho }) => {
                         <li className='flex justify-between mt-4 font-bold'>
                             <span>Total:</span>
                             <span>${calcularTotal()}</span>
+                        </li>
+                        <li className='mt-4'>
+                            <div className="p-inputgroup">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-map-marker"></i>
+                                </span>
+                                <InputText placeholder="Endereço" value={endereco} onChange={(e) => setEndereco(e.target.value)} className="p-d-block" />
+                            </div>
+                        </li>
+                        <li className='mt-2'>
+                            <div className="p-inputgroup">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-map"></i>
+                                </span>
+                                <InputText placeholder="Cidade" value={cidade} onChange={(e) => setCidade(e.target.value)} className="p-d-block" />
+                            </div>
                         </li>
                         <li className='mt-4'>
                             <a href={buildWhatsAppLink()} target="_blank" rel="noopener noreferrer">
