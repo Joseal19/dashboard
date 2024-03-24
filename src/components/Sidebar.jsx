bc755f12731f351818bf564c39981cc99911b5a2
Joseal19 <albertoj0988@gmail.com>
2024-03-24 13:00:35 -0300

    Loja

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,9 @@
 
 import React from 'react'; 
 import { Menubar } from 'primereact/menubar';
-import { Avatar } from 'primereact/avatar';  
 
 export default function BasicDemo() {
     const items = [
-        {
-            label: 'Graficos',
-            icon: 'pi pi-chart-bar',
-            url: '#graficos',
-            id: 1
-        },
         {
             label: 'Produtos',
             icon: 'pi pi-shopping-bag',
@@ -20,7 +13,6 @@ export default function BasicDemo() {
     ];
     const end = (
         <div className="flex align-items-center gap-2">
-            <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
         </div>
     );
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Graphic from '../components/Graphic';
 import Carousel from '../components/CarouselActios';
-import Line from '../components/Line';
-import ProductList from '../components/ProductList';
 import {ToastProvider}  from '../components/ToastProvider';
 
 const Home = () => {
@@ -12,14 +9,7 @@ const Home = () => {
         <div className='col-span-1 sm:col-span-2 mt-5 z-30'>
           <Carousel />
         </div>
-        <div id='graficos' className='sm:col-span-1'>
-          <Graphic />
-        </div>
-        <div className='sm:col-span-1'>
-          <Line />
-        </div>
         <div id='produtos' className='col-span-1 sm:col-span-2'>
-          <ProductList />
         </div>
       </div>
     </ToastProvider>
